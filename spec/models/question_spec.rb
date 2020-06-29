90728ac04d4b72774761e873afdd61b39969a404
gregoryawesoman <ray_couper@mail.ru>
2020-06-29 14:56:54 +0300

    added dependent option to question

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it { should have_many(:answers) }
+  it { should have_many(:answers).dependent(:destroy) }
 
   describe 'validations' do
     it { should validate_presence_of :title }
