b39c42bc7855999820cd7c040fe80e9d25ad429a
gregoryawesoman <ray_couper@mail.ru>
2020-07-16 23:12:27 +0300

    code from screencast: active storage - add multiple files to question

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -13,4 +13,8 @@ RSpec.describe Question, type: :model do
     it { should have_db_column(:body).of_type(:text).with_options(null: false) }
   end
 
+  it 'should has many attached files' do
+    expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+  end
+
 end
