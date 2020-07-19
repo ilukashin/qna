cca33918d54ddde54942e8e82f94f888c542d591
gregoryawesoman <ray_couper@mail.ru>
2020-07-19 15:04:39 +0300

    code from screencast: user story - added nested links to questions

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -40,6 +40,10 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'assigns a new Question to @question' do
       expect(assigns(:question)).to be_a_new(Question)
+      end
+
+    it 'assigns a new link to @link' do
+      expect(assigns(:question).links.first).to be_a_new(Link)
     end
 
     it 'renders #new view' do
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
+  it { should have_many(:links).dependent(:destroy) }
 
   describe 'validations' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :body }
   end
+
+  it { should accept_nested_attributes_for :links }
 
   describe 'table rules' do
     it { should have_db_column(:title).of_type(:string).with_options(null: false) }
