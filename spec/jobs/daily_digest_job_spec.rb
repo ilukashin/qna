9b591af0139b312dd9f80df6571ad49da1cd0321
gregoryawesoman <ray_couper@mail.ru>
2020-11-15 12:57:03 +0300

    typo fix

--- spec/jobs/daily_digest_job_spec.rb
+++ spec/jobs/daily_digest_job_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe DailyDigestJob, type: :job do
     allow(DailyDigestService).to receive(:new).and_return(service)
   end
 
-  it 'calls DailyDigestService#calculate' do
+  it 'calls DailyDigestService#send_digest' do
     expect(service).to receive(:send_digest)
     DailyDigestJob.perform_now
   end
